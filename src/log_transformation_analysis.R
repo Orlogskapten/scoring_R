099891651dd811ef5783a1bd7d868fe3d84ca967
Orlogskapten <wenceslas.sanchez@gmail.com>
2020-03-23 21:08:26 +0100

    Log transformation analysis ending

--- src/log_transformation_analysis.R
+++ src/log_transformation_analysis.R
@@ -3,11 +3,14 @@ rm(list = ls()) # clean env
 DATA_SOURCE = "C:/Users/Wenceslas/Desktop/R/R_project/scoring/projecto/scoring_R/data/no_na_dataset.csv"
 
 
+library(gridExtra)
+
 data= read.csv(DATA_SOURCE)
 head(data)
 
 data$BAD= as.factor(data$BAD)
 str(data)
+
 
 # Figure size management 
 fig <- function(width, heigth){
@@ -45,12 +48,45 @@ qqploting_gr(data, "YOJ")
 data_log= data
 col= c("LOAN", "VALUE", "MORTDUE")
 data_log[, col] = log(data_log[, col])
+# YOJ contain value = 0, we are going to make log(1+YOJ)
 data_log$YOJ= log(data_log$YOJ + 1)
 
-# YOJ contain value = 0, we are going to make log(1+YOJ)
 qqploting(data_log, "LOAN")
 qqploting(data_log, "VALUE")
 qqploting(data_log, "MORTDUE")
 qqploting(data_log, "YOJ")
 
 
+#### Final save for .doc ####
+hist_plot= function(data, variable_name, color= "orange"){
+  ggplot2::ggplot(as.data.frame(data)
+                          , ggplot2::aes(x= data[, c(variable_name)])
+                          , na.rm= T
+  ) +
+    ggplot2::geom_histogram(col= "black", fill= color, ggplot2::aes(y= ..density..), bins= 50) +
+    ggplot2::stat_function(fun = dnorm, colour = "black", size= 1.8,
+                  args = list(mean = mean(data[, c(variable_name)], na.rm = TRUE),
+                              sd = sd(data[, c(variable_name)], na.rm = TRUE)
+                              )
+                  ) +
+    ggplot2::xlab(variable_name)
+}
+
+# Visualize our merged plot
+fig(8, 10)
+grid.arrange(qqploting(data, "LOAN"), hist_plot(data, "LOAN"), qqploting(data_log, "LOAN"), hist_plot(data_log, "LOAN")
+             , qqploting(data, "VALUE"), hist_plot(data, "VALUE"), qqploting(data_log, "VALUE"), hist_plot(data_log, "VALUE")
+             , qqploting(data, "MORTDUE"), hist_plot(data, "MORTDUE"), qqploting(data_log, "MORTDUE"), hist_plot(data_log, "MORTDUE")
+             , qqploting(data, "YOJ"), hist_plot(data, "YOJ"), qqploting(data_log, "YOJ"), hist_plot(data_log, "YOJ")
+             , ncol= 4, nrow= 4
+)
+
+# Save the figure
+g= arrangeGrob(qqploting(data, "LOAN"), hist_plot(data, "LOAN"), qqploting(data_log, "LOAN"), hist_plot(data_log, "LOAN")
+               , qqploting(data, "VALUE"), hist_plot(data, "VALUE"), qqploting(data_log, "VALUE"), hist_plot(data_log, "VALUE")
+               , qqploting(data, "MORTDUE"), hist_plot(data, "MORTDUE"), qqploting(data_log, "MORTDUE"), hist_plot(data_log, "MORTDUE")
+               , qqploting(data, "YOJ"), hist_plot(data, "YOJ"), qqploting(data_log, "YOJ"), hist_plot(data_log, "YOJ")
+               , ncol= 4, nrow= 4
+)
+
+ggplot2::ggsave("C:/Users/Wenceslas/Desktop/R/R_project/scoring/projecto/scoring_R/images/log_qqplot.png", g, width= 10, height= 8)
